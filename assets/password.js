0b1fa203e621ff61400008655648beba2172ea43
Alex Noble-James <alexnoble143.an@gmail.com>
2022-01-11 21:39:26 -0500

    added style sheet and got the PW characters working. Need to fix false character responses

--- assets/password.js
+++ assets/password.js
@@ -1,8 +1,10 @@
-alert ("Click Generate Passowrd to select your password criteria")
 
-var passwordLength = generatePassword(passwordLength);
+//var passwordLength = passwordLength;
 
-function generatePassword(passwordLength) {
+alert ("Click Generate Password to select your password criteria")
+
+function generatePassword() {
+  
   var passwordLength = window.prompt("Specify the character length of your password. Enter a number.");
   passwordLength = parseInt(passwordLength)
   if (passwordLength < 8) {
@@ -20,21 +22,18 @@ function generatePassword(passwordLength) {
   else {
     console.log('length of password is ' + passwordLength + ' characters.');
     charactersIncluded();
-    
   }
   return passwordLength;
 }
 
 
 
-generatePassword();
+//generatePassword();
   
-
 function charactersIncluded() {
   alert('Please specify the character types to be included in your password');
-  function lowercase(characterLowercaseResponse) {
+  //function lowercase(characterLowercaseResponse) {
     var characterLowercaseResponse = window.confirm("Would you like lowercase characters?");
-    // characterResponse = characterResponse.toLowerCase
     if (characterLowercaseResponse) {
       console.log("include lowercase");
       characterLowercaseResponse = true;
@@ -42,13 +41,12 @@ function charactersIncluded() {
       console.log("no lowercase");
       characterLowercaseResponse = false;
     }
-    uppercase();
-  }
+    //uppercase();
+  //}
+debugger
 
-
-  function uppercase(characterUppercaseResponse) {
+  //function uppercase(characterUppercaseResponse) {
     var characterUppercaseResponse = window.confirm("Would you like uppercase characters?");
-    // characterResponse = characterResponse.toLowerCas.e
     
     if (characterUppercaseResponse) {
       console.log("include uppercase");
@@ -57,12 +55,11 @@ function charactersIncluded() {
       console.log("no uppercase");
       characterUppercaseResponse = false;
     }
-    special();
-  }
+    //special();
+  //}
 
-  function special(characterSpecialResponse) {
+  //function special(characterSpecialResponse) {
     var characterSpecialResponse = window.confirm("Would you like special characters?");
-    // characterResponse = characterResponse.toLowerCas.e
     if (characterSpecialResponse) {
       console.log("include special characters")
       characterSpecialResponse = true;
@@ -70,9 +67,9 @@ function charactersIncluded() {
       console.log("no special characters")
       characterSpecialResponse = false
     }
-  }
+  //}
 
-  if (lowercase() && uppercase() && special() == false) {
+  if (characterLowercaseResponse && characterUppercaseResponse && characterSpecialResponse == false) {
     alert("You must choose at least ONE criterion, please try again");
     charactersIncluded();
   }
@@ -82,11 +79,13 @@ function charactersIncluded() {
     finalPassword();
   }
 
-  var characterLowercaseResponse = lowercase(characterLowercaseResponse);
-  var characterUppercaseResponse = uppercase(characterUppercaseResponse);
-  var characterSpecialResponse = special(characterSpecialResponse);
+  //var characterLowercaseResponse = lowercase(characterLowercaseResponse);
+  //var characterUppercaseResponse = uppercase(characterUppercaseResponse);
+  //var characterSpecialResponse = special(characterSpecialResponse);
 }
 
+// values revert to being undefined in the console
+// need a function to actually create the password with the values that I took.
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
